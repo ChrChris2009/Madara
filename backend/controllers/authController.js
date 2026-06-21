9765c631cb2323de8323a6978385dd0812fc3604
Chris Stars <145717468+ChrChris2009@users.noreply.github.com>
2026-06-21 11:57:36 +0100

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+// INSCRIPTION
 exports.register = async (req, res) => {
     try {
         const { username, email, password } = req.body;
@@ -21,6 +22,7 @@ exports.register = async (req, res) => {
     }
 };
 
+// CONNEXION
 exports.login = async (req, res) => {
     try {
         const { email, password } = req.body;
@@ -30,10 +32,10 @@ exports.login = async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(400).json({ message: "Identifiants invalides." });
 
-        const token = jwt.sign({ id: user._index }, process.env.JWT_SECRET, { expiresIn: '1d' });
+        // Correction ici : changement de user._index en user._id
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
         res.json({ token, user: { id: user._id, username: user.username, email: user.email } });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 };
-
