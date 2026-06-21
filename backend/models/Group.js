942c9ab5d2b2d82eca98ea0b7dddd483f9f6db1b
Chris Stars <145717468+ChrChris2009@users.noreply.github.com>
2026-06-21 10:14:22 +0100

    Create Group.js

---
+++ backend/models/Group.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const GroupSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    avatar: { type: String, default: '' },
+    members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Group', GroupSchema);
+
